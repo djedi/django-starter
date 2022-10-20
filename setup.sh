9c008b241db868144e81eb03f112cf78e6f8161d
Dustin Davis <dustin.davis@edgecast.com>
2022-10-20 12:57:39 -0600

    a

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ read -p "Enter project name: " new_project_name
 echo "renaming project to $new_project_name"
 
 # rename project
-find . -name '*.py' -exec sed "s/projectname/${new_project_name}/g" {} \;
+find . -name '*.py' -exec sed -i -e "s/projectname/${new_project_name}/g" {} \;
+find . -name '*.py-e' -delete
 mv projectname $new_project_name
 
 # copy .env variables

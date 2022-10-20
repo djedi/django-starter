248f7246382d69519fe9223b959f4df75adaa597
Dustin Davis <dustin.davis@edgecast.com>
2022-10-20 15:18:58 -0600

    getting there...

--- projectname/settings/dev.py
+++ projectname/settings/dev.py
@@ -7,3 +7,11 @@ from . import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ read -p "Enter main project folder name: " new_project_name
 echo "renaming project to $new_project_name"
 
 # rename project
-find . -name '*.py' -exec sed -i "" -e "s/projectname/${new_project_name}/g" {} \;
+find . -iname '*.py' -o -iname '*.env' -exec sed -i "" -e "s/projectname/${new_project_name}/g" {} \;
 mv projectname $new_project_name
 
 # copy .env variables

cd3f30a2978266e3b4c2bce1702915124bd35444
Dustin Davis <dustin.davis@edgecast.com>
2022-10-20 12:51:25 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Djedi's Django Starter
+
+## Setup
+
+1. Clone this repo.
+
+   ```shell
+   git clone <url>
+   ```
+
+1. Run setup script.
+
+   ```shell
+   ./setup.sh
+   ```
--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 read -p "Enter project name: " new_project_name
 echo "renaming project to $new_project_name"
 
-find . -name '*.py' -exec sed -i -e "s/projectname/${new_project_name}/g" {} \;
+# rename project
+find . -name '*.py' -exec sed "s/projectname/${new_project_name}/g" {} \;
+mv projectname $new_project_name
 
 # copy .env variables
 cp dev.env .env
